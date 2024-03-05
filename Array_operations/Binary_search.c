0a3812b35a5922d3e7bc8f84bb9b23627a18c773
Gaurav Kumar <160252955+KumarGaurav007@users.noreply.github.com>
2024-03-05 17:38:39 +0530

    Update Binary_search.c

--- Array_operations/Binary_search.c
+++ Array_operations/Binary_search.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2; // calculating mid 
+
+        // Check if x is present at mid
+        if (arr[mid] == target)
+            return mid;
+
+        // If x greater, ignore left half
+        if (arr[mid] < target)
+            left = mid + 1;
+
+        // If x is smaller, ignore right half
+        else
+            right = mid - 1;
+    }
+
+    // if we reach here, then element was not present
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+    int result = binarySearch(arr, 0, n - 1, target);
+    // checking condition using ternary operator
+    (result == -1) ? printf("Element is not present in array") : printf("Element is present at index %d", result);
+
+    return 0;
+}
 
